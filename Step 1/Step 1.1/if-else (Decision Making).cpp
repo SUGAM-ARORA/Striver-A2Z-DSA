60e6eb82a4ac8df846dad0bf52df86c7dd9b481d
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-02-23 22:28:43 +0530

    Create if-else (Decision Making).cpp

---
+++ Step 1/Step 1.1/if-else (Decision Making).cpp
@@ -0,0 +1,18 @@
+//User function Template for C++
+
+class Solution {
+  public:
+    string compareNM(int n, int m){
+        // code here
+        if(n<m)
+        {
+            cout<<"lesser";
+        }
+        else if (n==m)
+        {
+            cout<<"equal";
+        }
+        else
+        cout<<"greater";
+    }
+};
