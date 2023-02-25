3491a653b023ec53d5eef1e3f5905b00339fae8f
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-02-25 15:35:49 +0530

    Create Pattern 5.cpp

---
+++ Step 1/Step 1.2/Pattern 5.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=0;i<n;i++)
+  {
+    for(int j=i;j<n;j++)
+    {
+      cout<<"*"<<" ";
+    }
+    cout<<endl;
+}
+	}
+};
