0b6bbba07d2a5d977e06ba6dd9158ed4536c8afb
Sugam Arora <96546088+SUGAM-ARORA@users.noreply.github.com>
2023-02-26 16:43:27 +0530

    Create Pattern 21.cpp

---
+++ Step 1/Step 1.2/Pattern 21.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        for(int i=1;i<=n;i++)
+
+        {
+
+            for(int j=1;j<=n;j++)
+
+                if(i==1 || i==n || j==1 || j==n)
+
+                    cout<<"*";
+
+                else
+
+                    cout<<" ";
+
+            cout<<endl;
+
+       }
+    }
+};
